f21f21308276a968d33d4ae15d1692699cac56b3
Mohammadreza Berneti <mberneti@live.com>
2019-12-07 13:28:57 +0330

    Updates

---
+++ precache-manifest.c7cb757185549361dc51b93d91edb226.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44efadfd3c2ec035f0ae6b61d49a388",
+    "url": "/sandbox/index.html"
+  },
+  {
+    "revision": "645987c76c44a89dfe8c",
+    "url": "/sandbox/static/js/2.de1be955.chunk.js"
+  },
+  {
+    "revision": "49309882d0acd110106c7be3de4100a8",
+    "url": "/sandbox/static/js/2.de1be955.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f319473ec0ba8bee9abd",
+    "url": "/sandbox/static/js/main.eebbd2fb.chunk.js"
+  },
+  {
+    "revision": "3b019b5eb833695f06ef",
+    "url": "/sandbox/static/js/runtime-main.63efd1c9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sandbox/precache-manifest.acf08eca85e0cdc204a44755f21f0476.js"
+  "/sandbox/precache-manifest.c7cb757185549361dc51b93d91edb226.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sandbox/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
